f4711a7a2ab13d5b82e0fc63ad64cf38dbb22cff
Rajat Mehta <imrajatmehta@gmail.com>
2023-07-29 13:21:46 +0530

    mend

--- composite_consumer_barrier.go
+++ composite_consumer_barrier.go
@@ -1,3 +1,7 @@
+// Below code is implemented coz for a ease of producer.
+// As producer will need to check the slow pointer from consumers before writting to buffer,
+// if we dont implement it saperately then producer will need to write
+// the below Load Fn logic in there side.
 package disruptor
 
 import "math"
@@ -7,6 +11,8 @@ type compositeConsumerBarrier []*Sequence
 func NewCompositeConsumerBarrier(sequences ...*Sequence) Barrier {
 	return compositeConsumerBarrier(sequences)
 }
+
+// Return the slow sequence/cursor/index from all consumers.
 func (sequences compositeConsumerBarrier) Load() int64 {
 	var min int64 = math.MaxInt64
 	for i := range sequences {
